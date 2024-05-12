62c19c2ff29e75c42368873ca0c8e063c748d2f4
Ali-hassan23 <127784171+Ali-hassan23@users.noreply.github.com>
2024-05-12 10:28:06 +0500

    backend me b staff editted

--- backend/routes/staffRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pool = require("../db");
-const { viewAllStaff } = require("../controller/staff");
-
-router.get("/",viewAllStaff);
-
-module.exports = router;
