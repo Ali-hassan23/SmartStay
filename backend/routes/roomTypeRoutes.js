0d6bb15b6fc6bec50527e2f56f533873021e6be7
Ali-hassan23 <127784171+Ali-hassan23@users.noreply.github.com>
2024-05-16 11:20:39 +0500

    room type route added

--- backend/routes/roomTypeRoutes.js
+++ backend/routes/roomTypeRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require("../db");
 const { getAllRoomTypes } = require("../controller/roomType");
 
-router.get("/",getAllRoomTypes);
+router.get("/", getAllRoomTypes);
 
 module.exports = router;
-
